e499da4ea1b056f7df52e914b973e39d82f0908e
YongA <ji5919@naver.com>
2023-06-14 13:29:08 +0900

    feat: Initial commit

---
+++ src/pages/Main.js
@@ -0,0 +1,11 @@
+import Header from "components/common/Header";
+import Layout from "components/common/Layout";
+import React from "react";
+
+export default function Main() {
+  return (
+    <Layout>
+      <Header />
+    </Layout>
+  );
+}
